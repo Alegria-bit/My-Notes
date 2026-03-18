1a8cabcb6681fcb1af86182d0aea4e57542b49c2
oskrip <olgask4@gmail.com>
2026-03-18 13:54:30 -0400

    init commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import WbIncandescentTwoToneIcon from "@mui/icons-material/WbIncandescentTwoTone";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <WbIncandescentTwoToneIcon /> My Notes
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
